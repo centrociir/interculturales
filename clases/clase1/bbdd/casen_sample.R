f1d663c2fc009bedcaf9490887e7f8dd6a05e59a
Matías Deneken <matdknu@gmail.com>
2025-05-03 17:04:20 +0100

    up-clase1_html

--- clases/clase1/bbdd/casen_sample.R
+++ clases/clase1/bbdd/casen_sample.R
@@ -1,10 +1,26 @@
 library(haven)
+library(tidyverse)
+library(sjmisc)
+
+casen2022 <- read_dta("clases/clase1/bbdd/casen2022.dta")
 
 casen2022_sample <- casen2022 %>%
-  sample_n(2000) |> select(region, nse, sexo, ecivil, educ, pueblos_indigenas,
-                          pobreza, yautcor)
+  sample_n(2000) |> select(region, 
+                           nse, 
+                           sexo, 
+                           ecivil,
+                           educ,
+                           pueblos_indigenas,
+                          pobreza,
+                          p9, 
+                          edad, 
+                          yautcor, 
+                          ind_hacina)
+
+
+
 
 getwd()
 write_rds(casen2022_sample, "clases/clase1/bbdd/casen2022_sample.rds")
 write_rds(casen2022_sample, "clases/clase1/pres/data/casen2022_sample.rds")
-
+write_csv(casen2022_sample, "clases/clase1/bbdd/casen2022_sample.csv")
