65e4ebbe8703c13d5a71223b4b1e78fd12fcb215
Matías Deneken <matdknu@gmail.com>
2025-05-22 23:57:25 +0100

    up-codigo

--- clases/clase3/practico/codigo_practico3.R
+++ clases/clase3/practico/codigo_practico3.R
@@ -20,10 +20,7 @@ library(readxl)           # Lectura de archivos Excel
 
 
 bbdd <- read_csv("https://raw.githubusercontent.com/centrociir/interculturales/refs/heads/main/clases/clase3/practico/bbdd/bbdd.csv")  
-  
-
-bbdd <- read_excel("clases/clase3/practico/bbdd/bbdd.xlsx")  # Base con datos de presidentas
-write.csv(bbdd, "clases/clase3/practico/bbdd/bbdd.csv", row.names = FALSE)  # Guarda la base como CSV
+  write.csv(bbdd, "clases/clase3/practico/bbdd/bbdd.csv", row.names = FALSE)  # Guarda la base como CSV
 
 world <- ne_countries(scale = "medium", returnclass = "sf")   # Mapa mundial con geometría en formato sf
 
